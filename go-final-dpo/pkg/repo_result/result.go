dcb3e4ab82a5ae4e58716c20f8237a7c61cbd064
Sergey Kisil <9161115691@mail.ru>
2022-11-10 19:49:50 +0300

    refactor

--- go-final-dpo/pkg/repo_result/result.go
+++ go-final-dpo/pkg/repo_result/result.go
@@ -8,6 +8,7 @@ import (
 	"Diplom/go-final-dpo/pkg/repo_sms"
 	"Diplom/go-final-dpo/pkg/repo_support"
 	"Diplom/go-final-dpo/pkg/repo_voice"
+	"sync"
 )
 
 type ResultSetT struct {
@@ -27,41 +28,94 @@ type ResultT struct {
 }
 
 func getResultData() *ResultSetT {
-
+	var wg sync.WaitGroup
 	var result *ResultSetT
+	var smsData [][]*repo_sms.SMSData
+	var mmsData [][]*repo_mms.MMSData
+	var voiceCallData []*repo_voice.VoiceData
+	var emailData map[string][][]*repo_email.EmailData
+	var billingData *repo_billing.BillingData
+	var supportData []int
+	var incidentsData []*repo_incident.IncidentData
+	wg.Add(7)
 
-	SD := repo_sms.NewStorageSMS()
-	SD.ReadFileSMS()
-	MD := repo_mms.NewStorageMMS()
-	MD.GetMMS()
-	VD := repo_voice.NewStorageVoice()
-	VD.ReadFileVoice()
-	ED := repo_email.NewStorageEmail()
-	ED.ReadFileEmail()
-	BD := repo_billing.NewStorageBilling()
-	BD.ReadFileBilling()
-	SSD := repo_support.NewStorageSupport()
-	SSD.GetSupport()
-	ID := repo_incident.NewStorageIncident()
-	ID.GetIncident()
+	go func() {
+		defer wg.Done()
+		SD := repo_sms.NewStorageSMS()
+		SD.ReadFileSMS()
+		smsData = repo_sms.SortedSMSData(SD)
+	}()
+	go func() {
+		defer wg.Done()
+		MD := repo_mms.NewStorageMMS()
+		MD.GetMMS()
+		mmsData = repo_mms.SortedMMSData(MD)
+	}()
+
+	go func() {
+		defer wg.Done()
+		VD := repo_voice.NewStorageVoice()
+		VD.ReadFileVoice()
+		voiceCallData = repo_voice.AddVoiceData(VD)
+	}()
+
+	go func() {
+		defer wg.Done()
+		ED := repo_email.NewStorageEmail()
+		ED.ReadFileEmail()
+		emailData = repo_email.SortedEmailData(ED)
+	}()
+
+	go func() {
+		defer wg.Done()
+		BD := repo_billing.NewStorageBilling()
+		BD.ReadFileBilling()
+		billingData = BD[0]
+	}()
+
+	go func() {
+		defer wg.Done()
+		SSD := repo_support.NewStorageSupport()
+		SSD.GetSupport()
+		supportData = repo_support.SupportDataStatus(SSD)
+	}()
+
+	go func() {
+		defer wg.Done()
+		ID := repo_incident.NewStorageIncident()
+		ID.GetIncident()
+		incidentsData = repo_incident.AddIncidentData(ID)
+	}()
+
+	wg.Wait()
 
 	result = &ResultSetT{
-		SMS:       repo_sms.SortedSMSData(SD),
-		MMS:       repo_mms.SortedMMSData(MD),
-		VoiceCall: repo_voice.AddVoiceData(VD),
-		Email:     repo_email.SortedEmailData(ED),
-		Billing:   BD[0],
-		Support:   repo_support.SupportDataStatus(SSD),
-		Incidents: repo_incident.AddIncidentData(ID),
+		SMS:       smsData,
+		MMS:       mmsData,
+		VoiceCall: voiceCallData,
+		Email:     emailData,
+		Billing:   billingData,
+		Support:   supportData,
+		Incidents: incidentsData,
 	}
 
 	return result
 }
 
-func GetResult() ResultT {
-	return ResultT{
-		Status: true,
-		Data:   *getResultData(),
-		Error:  "",
+func GetResult() *ResultT {
+	var result *ResultT
+	r := getResultData()
+	if len(r.SMS[0]) == 0 || len(r.MMS[0]) == 0 || len(r.VoiceCall) == 0 || len(r.Email) == 0 || r.Support[0] == 0 || len(r.Incidents) == 0 {
+		result = &ResultT{
+			Status: false,
+			Error:  "Ошибка получения данных",
+		}
+	} else {
+		result = &ResultT{
+			Status: true,
+			Data:   *r,
+			Error:  "",
+		}
 	}
+	return result
 }
